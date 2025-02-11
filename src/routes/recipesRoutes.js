5406e97382f3bf86218d730255dea3e11f6b7172
ItsMen <mvdo1091@gmail.com>
2025-02-11 00:54:52 -0500

    updated schema for swagger post/put

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -1,3 +1,30 @@
+/**
+ * @swagger
+ * /recipes:
+ *   post:
+ *     summary: Create a new recipe
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               ingredients:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               instructions:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Recipe created successfully
+ *       400:
+ *         description: Invalid input data
+ */
+
 const express = require('express');
 const {
     getAllRecipes,
